c97bb18b56bc90397a4ccf2d56b52cfaeea998b9
BalamiRR <fuatkara.95@gmail.com>
2025-07-17 14:04:15 +0200

    New Scenarios and step definitions

--- support/hooks.ts
+++ support/hooks.ts
@@ -3,8 +3,10 @@ import { CustomWorld } from './world'
 
 Before(async function (this: CustomWorld) {
     await this.init()
+    await this.homePage.goto(this.baseURL)
 })
 
 After(async function (this: CustomWorld) {
     await this.close()
 })
+
